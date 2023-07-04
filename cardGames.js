f172fd04ac3235d63cf07615697c1eb369bc0d34
ELDAR mAGERRAMOV <magerramov.en@gmail.com>
2023-07-04 18:46:32 +1100

    modified:   cardGames.js 	modified:   index.html 	modified:   style.css

--- cardGames.js
+++ cardGames.js
@@ -1,20 +1,14 @@
-const hardButton = document.querySelectorAll(".hard-level-select-button");
+const hardInput = document.querySelectorAll(".hard-level-select-input");
 const startButton = document.querySelector(".start-game-button");
 const hardLevelElem = document.querySelector(".hard-level-elem");
 const containeer = document.querySelector(".containeer");
-console.log(hardButton);
+
+hardInput.forEach((elem) => {
+  elem.addEventListener("click", (event) => {
+    console.log(elem.value);
+  });
+});
+
 startButton.addEventListener("click", (event) => {
   containeer.removeChild(hardLevelElem);
-});
-hardButton.forEach((elem) => {
-  elem.addEventListener("click", (event) => {
-    let level = elem.textContent;
-    if (level === "1") {
-      elem.classList.add("clicked");
-    } else if (level === "2") {
-      elem.classList.add("clicked");
-    } else {
-      elem.classList.add("clicked");
-    }
-  });
 });
