9ee212e03ea854df0daa43fbe9e217c98f189e84
ELDAR mAGERRAMOV <magerramov.en@gmail.com>
2023-07-09 11:06:16 +1100

    modified:   cardGames.js 	new file:   "img/10 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/10 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/10 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/10 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/6 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/6 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/6 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/6 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/7 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/7 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/7 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/7 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/8 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/8 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/8 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/8 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/9 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/9 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/9 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/9 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/\320\262\320\260\320\273\320\265\321\202 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/\320\262\320\260\320\273\320\265\321\202 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/\320\262\320\260\320\273\320\265\321\202 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/\320\262\320\260\320\273\320\265\321\202 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/\320\264\320\260\320\274\320\260 \320\261\321\203\320\261\320\275\321\213.svg"     	new file:   "img/\320\264\320\260\320\274\320\260 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/\320\264\320\260\320\274\320\260 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/\320\264\320\260\320\274\320\260 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/\320\272\320\276\321\200\320\276\320\273\321\214 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/\320\272\320\276\321\200\320\276\320\273\321\214 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/\320\272\320\276\321\200\320\276\320\273\321\214 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/\320\272\320\276\321\200\320\276\320\273\321\214 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   "img/\321\200\321\203\320\261\320\260\321\210\320\272\320\260.svg" 	new file:   "img/\321\202\321\203\320\267 \320\261\321\203\320\261\320\275\321\213.svg" 	new file:   "img/\321\202\321\203\320\267 \320\272\321\200\320\265\321\201\321\202\320\270.svg" 	new file:   "img/\321\202\321\203\320\267 \320\277\320\270\320\272\320\270.svg" 	new file:   "img/\321\202\321\203\320\267 \321\207\320\265\321\200\320\262\320\270.svg" 	new file:   indexheader.html 	modified:   style.css

--- cardGames.js
+++ cardGames.js
@@ -4,7 +4,40 @@ const hardLevelElem = document.querySelector(".hard-level-elem");
 const containeer = document.querySelector(".containeer");
 const form = document.querySelector("form");
 
-console.log(hardInputs);
+const gameHeader = document.createElement("div");
+const timer = document.createElement("div");
+const timerCount = document.createElement("p");
+
+const min = document.createElement("div");
+const minText = document.createElement("p");
+
+const sec = document.createElement("div");
+const secText = document.createElement("p");
+
+const gameRestartButton = document.createElement("button");
+
+gameHeader.classList.add("game-header");
+timer.classList.add("timer");
+min.classList.add("min");
+minText.classList.add("min-text");
+sec.classList.add("sec");
+secText.classList.add("sec-text");
+gameRestartButton.classList.add("game-restart");
+gameRestartButton.classList.add("start-game-button");
+
+gameHeader.appendChild(timer);
+gameHeader.appendChild(gameRestartButton);
+timer.appendChild(min);
+min.appendChild(minText);
+timer.appendChild(sec);
+timer.appendChild(timerCount);
+sec.appendChild(secText);
+
+gameRestartButton.textContent = "Начать заново";
+minText.textContent = "min";
+secText.textContent = "sec";
+timerCount.textContent = "00.00";
+window.level = "1";
 
 hardInputs.forEach((elem) => {
   elem.addEventListener("input", (event) => {
@@ -14,5 +47,13 @@ hardInputs.forEach((elem) => {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  containeer.removeChild(hardLevelElem);
+  containeer.innerHTML = "";
+  containeer.appendChild(gameHeader);
+  if (window.level === "1") {
+    console.log("1");
+  } else if (window.level === "2") {
+    console.log("2");
+  } else {
+    console.log("3");
+  }
 });
